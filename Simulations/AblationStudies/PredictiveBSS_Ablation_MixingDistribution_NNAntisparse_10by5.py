129370cc495ec90d55b5605204e7515cfe2eb08c
Bariscan Bozkurt <bbozkurt@hpc-gw2.hpc.swc.ucl.ac.uk>
2026-04-23 13:41:54 +0000

    simulations update

--- Simulations/AblationStudies/PredictiveBSS_Ablation_MixingDistribution_NNAntisparse_10by5.py
+++ Simulations/AblationStudies/PredictiveBSS_Ablation_MixingDistribution_NNAntisparse_10by5.py
@@ -77,6 +77,7 @@ mixing_distribution_list = [
 predictivebss_hyperparam_dict = {
     "n_sources": NumberofSources,
     "presumed_domain": "nnantisparse",
+    "epsilon" : 1e-4,
     # Optimization parameters
     "lambda_lateral": 0.95,
     "gamma_predictive": 750,
